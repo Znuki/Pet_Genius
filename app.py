06f56a5b5d872240d1f9d4e4eae624d7aca63fd9
sooya333 <sooya333@naver.com>
2023-02-08 19:19:53 +0900

    이미지 저장 및 보여주기, 페이지간 연결

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from bson import Binary
 #  You need to convert binary image data to a base64 encoded string, then use that string to set the source of an img tag in HTML.
 import base64
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+# app = Flask(__name__)
 # app.config['TEMPLATES_AUTO_RELOAD'] = True
 # app.config['TEMPLATES_FOLDER'] = 'templates'
 
@@ -35,9 +36,9 @@ db = client.dbsparta
 # 각 페이지 연결 작업을 아직 못해서 각 페이지 기능 점검할 때 아래 3개 중 하나를 활성화해서 썼어요.
 @app.route('/')
 def home():
-    # return render_template('b_detail.html')
-    return render_template('write.html')
-    # return render_template('index.html')
+    # return render_template('detail.html')
+    # return render_template('write.html')
+    return render_template('index.html')
     # return render_template('comment.html')
 
 
@@ -61,48 +62,48 @@ def write_save():
     # insert the binary data and other information into MongoDB
     db.genius.insert_one({
         'num': num, 'category': category, 'role': role, 'region': region, 'name': name, 'title': title,
-        'content': content, 'time': time, "filename": file.filename
+        'content': content, 'time': time, "file": file.filename
     })
     # 실제 이미지 파일은 g_imgs 콜렉션에 저장
-    db.g_imgs.insert_one({'num': num, "file": binary_data})
+    db.g_imgs.insert_one({'num': num, "img": binary_data})
     return jsonify({'msg': '등록완료'})
+    # return render_template('detail.html', msg='등록완료')
 
 
 # db에서 모든 값을 가져와서 클라이언트로 보내주기
 @app.route("/board", methods=["GET"])
 def board_get():
-    # DB에서 모든 정보를 가져와서 변수 details에 넣어 주기
     details = list(db.genius.find({}, {'_id': False}))
+    img_binary_list = list(db.g_imgs.find({}, {'_id': False}))
+    img_string_list = []
+    for i in range(len(details)):
+        img_binary = img_binary_list[i]
+        img_string = base64.b64encode(img_binary['img']).decode("utf-8")
+        img_string_list.append(img_string)
+    if details == '' or img_string_list == '':
+        print("보드의 디테일즈 이미지스트링리스트 값없음")
+    else:
+        print("디테일, 인코디드 값 있음")
+    return jsonify({'details': details, 'img_string_list': img_string_list})
+    # return render_template('index.html', details=details, img_string_list=img_string_list)
 
-    # for i in range(len(details)):
-    #     # encoded_string = base64.b64encode(details[i]['file']).decode("utf-8")
-    #     encoded_string = details[i]['file'].encode("utf-8")
-    #     details[i]['file'] = encoded_string
-    # print(details)
-    # details 안의 값을 클라이언트에 전송하기
-    return jsonify({'details': details})
 
-
-# db에서 지정된 글의 내용을 가져와서 클라이언트(상세페이지)로 보내주기
-# 채원 님, 밑에 detail = db.genius.find_one({'num':'4'}, {'_id': False}) 보면, 제가 글번호(num)이 4인 글을 가져오게 해 놨어요.
-# 추후에 작성한 글, 클릭한 글의 num을 받아와서 넣어줄 수 있게 작업해야 해요. 같이 연구해 보아요.
 @app.route("/detail", methods=["GET"])
 def detail_get():
     num = request.args.get("num")
-    print("서버가 받은 넘버는 " + num)
-    # DB에서 해당 정보를 가져와서 변수 comments에 넣어 주기
+    print("디테일2 서버가 받은 넘버는 " + num)
     detail = db.genius.find_one({'num': num}, {'_id': False})
-    print(detail)
-    # print(type(detail)) - class 'dict'
-    img_file = db.g_imgs.find_one({'num': num}, {'_id': False})
-    # print(type(detail)) - class 'dict'
-
-    encoded_string = base64.b64encode(img_file['file']).decode("utf-8")
-    # detail에 file이라는 필드를 만들어 encoded_string을 넣기. 다른 데이터랑 같이 클라이언트에 넘길 수 있게
-    # detail['file'] = encoded_string
-    print("encoded_string:" + encoded_string)
-    return render_template('b_detail.html')
-    # return render_template('b_detail.html', detail=detail, encoded_string=encoded_string)
+    # print(detail)
+    img_binary = db.g_imgs.find_one({'num': num}, {'_id': False})
+    encoded_string = base64.b64encode(img_binary['img']).decode("utf-8")
+    if detail == '' or encoded_string == '':
+        print("detail2의 디테일 인코디드 스트링 값없음")
+    else:
+        print("디테일, 인코디드 값 있음")
+    # return jsonify({'detail': detail, 'encoded_string': encoded_string})
+    # return jsonify({'detail': detail, 'encoded_string': encoded_string})
+    # return render_template('detail.html', detail=detail)
+    return render_template('detail.html', detail=detail, encoded_string=encoded_string)
     # return jsonify({'detail': detail})
 
 
